f69e5c93e4086bddee8ff8e1bf6b1e61be763d99
Natalia <smieja.natalia@gmail.com>
2024-09-10 10:54:45 +0200

    changes in get requests

--- dictionary/crud.py
+++ dictionary/crud.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import Session
 from .database import get_db
 from .models import Description, Word
 from .schemas import (
+    AllWords,
     DescriptionModel,
     DescriptionReturn,
-    DictWord,
-    DictWordShort,
+    WordDescriptions,
     WordModel,
     WordReturn,
 )
@@ -20,65 +20,94 @@ router = APIRouter(prefix="/dict", tags=["dict"])
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.get("/all", response_model=None, status_code=200)
+@router.get(
+    "/words",
+    response_model=AllWords,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+    status_code=200,
+    description="Fetch all the data from the Words table.",
+)
 async def get_all_words(db: db_dependency):
     query = db.query(Word)
 
     if not query:
         raise HTTPException(404, "No words stored in the database.")
 
-    return {"number of words": query.count(), "words list": query.all()}
+    return {"number_of_words": query.count(), "words": query.all()}
 
 
-@router.get("/word/{id}", response_model=DictWord, status_code=200)
+@router.get(
+    "/word/translations",
+    response_model=None,
+    status_code=200,
+    description="Search for word translations by word ID or the word itself.",
+)
+async def get_word_translations(
+    db: db_dependency, id: int | None = None, search: str | None = None
+):
+    if not id and not search:
+        raise HTTPException(400, "Either 'id' or 'search' parameter must be given.")
+
+    if id:
+        word = db.query(Word).filter_by(id=id).first()
+        if not word:
+            raise HTTPException(404, f"No word with ID of {id} stored in the database.")
+
+        descriptions = db.query(Description).filter_by(word_id=word.id).all()
+        if not descriptions:
+            raise HTTPException(404, "No descriptions stored in the database.")
+
+        translation_list = [desc.in_polish for desc in descriptions]
+
+        return {"word": word.word, "translation": translation_list}
+
+    words = db.query(Word).filter(Word.word.icontains(search)).all()
+    if not words:
+        raise HTTPException(404, f"No word '{search}' stored in the database.")
+
+    results = []
+    for record in words:
+        descriptions = db.query(Description).filter_by(word_id=record.id).all()
+        translation_list = (
+            [desc.in_polish for desc in descriptions] if descriptions else []
+        )
+        results.append(
+            {
+                "word": {"word": record.word, "id": record.id},
+                "translation": translation_list,
+            }
+        )
+
+    return results
+
+
+@router.get(
+    "/word/{id}",
+    response_model=WordDescriptions,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+    status_code=200,
+    description="Get full information about the word by its ID.",
+)
 async def get_single_word(db: db_dependency, id: int):
     word = db.query(Word).filter_by(id=id).first()
 
     if not word:
-        raise HTTPException(404, "No word stored in the database.")
+        raise HTTPException(404, f"No word with ID of {id} stored in the database.")
 
     desc = db.query(Description).filter_by(word_id=id).all()
 
     return {"word": word, "description": desc}
 
 
-@router.get("/word/short/{id}", response_model=DictWordShort, status_code=200)
-async def get_single_word_translations(db: db_dependency, id: int):
-    word = db.query(Word).filter_by(id=id).first()
-
-    if not word:
-        raise HTTPException(404, "No word stored in the database.")
-
-    descriptions = db.query(Description).filter_by(word_id=id).all()
-    if not descriptions:
-        raise HTTPException(404, "No descriptions stored in the database.")
-
-    in_polish_list = [desc.in_polish for desc in descriptions]
-
-    return {"word": word.word, "description": in_polish_list}
-
-
-@router.get("/search", response_model=None, status_code=200)
-async def get_search_word_translation(db: db_dependency, word: str):
-    words = db.query(Word).filter(Word.word.icontains(word)).all()
-
-    if not words:
-        raise HTTPException(404, "No word stored in the database.")
-
-    print("✅✅✅", word)
-
-    desc_list = []
-    for word in words:
-        descriptions = db.query(Description).filter_by(word_id=word.id).all()
-        in_polish_list = [desc.in_polish for desc in descriptions]
-        desc_list.append(
-            {"word": {"word": word.word, "id": word.id}, "description": in_polish_list}
-        )
-
-    return desc_list
-
-
-@router.get("/description/{id}", response_model=DescriptionReturn, status_code=200)
+@router.get(
+    "/description/{id}",
+    response_model=DescriptionReturn,
+    response_model_exclude_none=True,
+    response_model_exclude_unset=True,
+    status_code=200,
+)
 async def get_single_description(db: db_dependency, id: int):
     desc = db.query(Description).filter_by(id=id).first()
 
@@ -89,7 +118,7 @@ async def get_single_description(db: db_dependency, id: int):
 
 
 @router.post("/new_word", response_model=WordReturn, status_code=201)
-async def add_new_word(db: db_dependency, new_word: WordModel):
+async def add_a_new_word(db: db_dependency, new_word: WordModel):
     model = new_word.model_dump()
     word = Word(**model)
 
@@ -100,7 +129,7 @@ async def add_new_word(db: db_dependency, new_word: WordModel):
 
 
 @router.post("/new_description/{id}", response_model=DescriptionReturn, status_code=201)
-async def add_new_description(db: db_dependency, id: int, new_desc: DescriptionModel):
+async def add_a_new_description(db: db_dependency, id: int, new_desc: DescriptionModel):
     word = db.query(Word).filter_by(id=id).first()
     if not word:
         raise HTTPException(404, "Word not found.")
